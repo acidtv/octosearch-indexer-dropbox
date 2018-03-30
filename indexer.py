154ae006c83c0ee3d77cd082df066521254e1737
Alex <alex@1200wd.com>
2018-03-30 20:32:23 +0200

    Updating to new indexer File object api
    and did some refactoring to make var names more clear

--- indexer.py
+++ indexer.py
@@ -1,6 +1,5 @@
 import dropbox
 from octosearch.indexer import File
-from io import TextIOWrapper
 
 
 class DropboxIndexer():
@@ -8,41 +7,50 @@ class DropboxIndexer():
     def index(self, conf):
         dbx = dropbox.Dropbox(conf['token'])
 
-        dirstack = [conf['path']]
+        folder_stack = [conf['path']]
 
-        while dirstack:
-            path = dirstack.pop()
+        while folder_stack:
+            folder = folder_stack.pop()
 
-            for entry in dbx.files_list_folder(path):
-                if isinstance(entry, dropbox.files.FolderMetadata):
-                    dirstack.append(entry.path_lower)
+            for object_metadata in dbx.files_list_folder(folder):
+                if isinstance(object_metadata, dropbox.files.FolderMetadata):
+                    folder_stack.append(object_metadata.path_lower)
                     continue
 
-                yield DropboxFile()
+                yield DropboxFile(dbx, object_metadata)
 
 
 class DropboxFile(File):
 
-    _response = None
+    _dbx = None
 
-    def __init__(self, file_metadata, response):
-        self._response = response
-        self._response.raw.decode_content = True
+    _file_metadata = None
 
-        super(DropboxFile, self).__init__(self._metadata(file_metadata))
+    _file = None
 
-    def open_binary(self):
-        return self._response.raw
+    def __init__(self, dbx, file_metadata):
+        super().__init__()
+        self._file_metadata = file_metadata
+        self._dbx = dbx
+        self._set_properties(file_metadata)
 
-    def open_text(self):
-        return TextIOWrapper(self.open_binary())
+    def open(self):
+        return self._file().raw
 
-    def _metadata(self, file_metadata):
-        return {
-            # 'title': bookmark.title,
-            'url': 'https://www.dropbox.com/home' + file_metadata.path_lower,
-            # 'extension': file_metadata.extension,
-            # 'mimetype': 'text/html',
-            'size': file_metadata.size,
-            'modified': file_metadata.server_modified
-        }
+    def _file(self):
+        if not self._file:
+            path = self._file_metadata.path_lower
+            self._file, response = self._dbx.files_download(path)
+
+            if response.code != 200:
+                raise Exception('Could not download file from dropbox, invalid response code')
+
+            # We don't want http `Content-Type: gzip` messing around here
+            self._file.raw.decode_content = True
+
+        return self._file
+
+    def _set_properties(self, file_metadata):
+        self.url = 'https://www.dropbox.com/home' + file_metadata.path_lower,
+        self.size = file_metadata.size,
+        self.modified = file_metadata.server_modified
